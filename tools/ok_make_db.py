76c4557aed721ea0621251d6401e4fd598c8ef50
animalize <malincns@163.com>
2017-04-08 22:39:51 +0800

    【】减少数据库尺寸

--- tools/ok_make_db.py
+++ tools/ok_make_db.py
@@ -4,14 +4,16 @@ import json
 import re
 import sqlite3
 from zipfile import ZipFile
+from collections import Counter
 
 empty_count = 0
 
 
 class Poem:
-    __slots__ = ('title', 'author', 'text')
+    __slots__ = ('id', 'title', 'author', 'text')
 
-    def __init__(self, title, author, text):
+    def __init__(self, id, title, author, text):
+        self.id = id
         self.title = title
         self.author = author
 
@@ -22,62 +24,29 @@ class Poem:
         self.text = text
 
     def get_tuple(self):
-        return (self.title.encode('utf_16_le'),
+        return (self.id,
+                self.title.encode('utf_16_le'),
                 self.author.encode('utf_16_le'),
                 self.text.encode('utf_16_le'))
 
     def __str__(self):
-        return '标题：%s\n作者：%s\n内容：\n%s\n' % (self.title, self.author, self.text)
-
-
-class Author:
-    __slots__ = ('author', 'desc')
-
-    def __init__(self, author, desc):
-        self.author = author
-        self.desc = desc
-
-    def get_tuple(self):
-        return (self.author.encode('utf_16_le'),
-                self.desc.encode('utf_16_le'))
+        return 'ID:%d\n标题：%s\n作者：%s\n内容：\n%s\n' % \
+                (self.id, self.title, self.author, self.text)
 
 
 def process_paragraphs(paragraphs):
     for i in range(0, len(paragraphs)):
-        m = re.search(r'-\d+-', paragraphs[i])
+        # 去掉 -2222-
+        m = re.search(r'-\d+-?', paragraphs[i])
         if m:
-            p = re.sub(r'-\d+-', r'', paragraphs[i])
-#             print(paragraphs[i])
-#             print(p)
-#             print()
+            p = re.sub(r'-\d+-?', r'', paragraphs[i])
             paragraphs[i] = p
+
+        # 只含。的行
         if paragraphs[i] == '。':
             paragraphs[i] = ''
 
     return '\n'.join(p for p in paragraphs if p != '')
-
-
-def load_author():
-    with open(r'json\authors.tang.json', encoding='utf-8') as f:
-        obj = json.load(f)
-
-    global author_set, author_table
-
-    lst = []
-    for d in obj:
-        author = d['name']
-        if author == 'JackeyGao':
-            continue
-
-        desc = d['desc']
-        if not desc.strip():
-            continue
-
-        a = Author(author, desc)
-        lst.append(a)
-
-    print('载入%d条作者' % len(lst))
-    return lst
 
 
 def load_poem():
@@ -105,13 +74,10 @@ def load_poem():
             text = process_paragraphs(paras)
 
             if not text:
-                #print(id, title, author)
                 global empty_count
                 empty_count += 1
-            # elif 'YY' in text:
-            #    print(id, text)
 
-            p = Poem(title, author, text)
+            p = Poem(id, title, author, text)
 
             m = re.search(r'[^\u0000-\uffff]', str(p))
             if m:
@@ -129,25 +95,25 @@ def create_db():
 
     # 建表
     sql = ('CREATE TABLE poem('
-           'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+           'id INTEGER PRIMARY KEY,'
            'title BLOB,'
            'author BLOB,'
            'txt BLOB);')
     db.execute(sql)
 
-#     sql = ('CREATE TABLE author('
-#            'author BLOB NOT NULL,'
-#            'info BLOB);')
-#     db.execute(sql)
-
-    # 索引
-    sql = 'CREATE INDEX author_idx ON poem(author);'
-    db.execute(sql)
-
-#     sql = 'CREATE INDEX author_idx2 ON author(author);'
-#     db.execute(sql)
-
     return db
+
+
+def save_close_db(lst, db):
+    sql = 'INSERT INTO poem VALUES(?,?,?,?);'
+    db.execute('BEGIN')
+    for p in lst:
+        db.execute(sql, p.get_tuple())
+    db.commit()
+
+    # 关闭数据库
+    db.execute('VACUUM')
+    db.close()
 
 
 def main():
@@ -162,30 +128,13 @@ def main():
     except Exception as e:
         print(e)
 
-    # 建数据库
-    db = create_db()
-
     # 诗
     lst = load_poem()
 
-    sql = 'INSERT INTO poem VALUES(NULL,?,?,?);'
-    db.execute('BEGIN')
-    for p in lst:
-        db.execute(sql, p.get_tuple())
-    db.commit()
-
-#     # 作者
-#     lst = load_author()
-#
-#     sql = 'INSERT INTO author VALUES(?,?);'
-#     db.execute('BEGIN')
-#     for a in lst:
-#         db.execute(sql, a.get_tuple())
-#     db.commit()
-
-    # 关闭数据库
-    db.execute('VACUUM')
-    db.close()
+    # 建数据库
+    db = create_db()
+    # 写数据库
+    save_close_db(lst, db)
 
     # zip
     with ZipFile('tangshi.db.zip', 'w') as myzip:
@@ -193,6 +142,5 @@ def main():
 
     print('无内容的记录%d条' % empty_count)
     print('运行完毕')
-
 
 main()
